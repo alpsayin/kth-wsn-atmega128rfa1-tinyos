7f1de3a121f22c7d0bf515bed3c7d3df6a90ff1e
alpsayin@gmail.com <alpsayin@gmail.com@ca3d9918-9570-4841-ee1b-e53f2f6ebe91>
2011-11-21 16:05:41 +0000

    sensor files junzhe tian

---
+++ modified-tinyos-main/tos/platforms/rcb128rfa1/PlatformSensorChannel.h
@@ -0,0 +1,15 @@
+#include "Atm128Adc.h"
+
+
+#define Sensor_Channel_0 ATM128_ADC_SNGL_ADC0		//Analog input channel 0-7
+#define Sensor_Channel_1 ATM128_ADC_SNGL_ADC1		//mapped to GPIO PF0-PF7
+#define Sensor_Channel_2 ATM128_ADC_SNGL_ADC2
+#define Sensor_Channel_3 ATM128_ADC_SNGL_ADC3
+#define Sensor_Channel_4 ATM128_ADC_SNGL_ADC4
+#define Sensor_Channel_5 ATM128_ADC_SNGL_ADC5
+#define Sensor_Channel_6 ATM128_ADC_SNGL_ADC6
+#define Sensor_Channel_7 ATM128_ADC_SNGL_ADC7
+
+#define Sensor_Channel_T ATM128_ADC_INT_TEMP		//internal temperature sensor output
+
+
