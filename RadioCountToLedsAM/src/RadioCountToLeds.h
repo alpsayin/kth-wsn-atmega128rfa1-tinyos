df1f8851f429e9a22630cddc010ff8586e00d453
alpsayin@gmail.com <alpsayin@gmail.com@ca3d9918-9570-4841-ee1b-e53f2f6ebe91>
2011-11-30 17:00:41 +0000

    amazing collection&dissemination protocols are amazing

--- RadioCountToLedsAM/src/RadioCountToLeds.h
+++ RadioCountToLedsAM/src/RadioCountToLeds.h
@@ -10,7 +10,7 @@
 #undefine TOS_NODE_ID
 #endif
 
-#define TOS_NODE_ID 4
+#define TOS_NODE_ID 2
 
 #ifdef DEFINED_TOS_AM_ADDRESS
 #undefine DEFINED_TOS_AM_ADDRESS
@@ -25,6 +25,24 @@
 #else
 #error set an actual _DEFINED_TOS_AM_ADDRESS
 #endif
+
+enum {
+	DATA_TYPE_TEMP = 1,
+	DATA_TYPE_LEDS
+};
+
+typedef nx_struct kth_wsn_command
+{
+	nx_uint8_t commandByte;
+	nx_uint8_t forReal;
+} kth_wsn_command_t;
+
+typedef nx_struct kth_wsn_data
+{
+	nx_uint8_t source;
+	nx_uint8_t type;
+	nx_uint16_t data;
+} kth_wsn_data_t;
 
 typedef nx_struct radio_count_msg {
   nx_uint16_t counter;
